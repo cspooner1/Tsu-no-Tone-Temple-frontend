fbc95b65ca39d9f53c6aa538320e8bca4060fd93
cspooner1 <christopherspooner423@gmail.com>
2023-06-22 02:31:21 -0500

    update

--- tsu-no-tone-temple/src/AnimeWorkout.js
+++ tsu-no-tone-temple/src/AnimeWorkout.js
@@ -1,7 +1,43 @@
-
+import { Card } from "react-bootstrap";
 
 function AnimeWorkout() {
-    return
+    return(
+        <>
+        <h1 style={{fontFamily:'fantasy'}}>Workout Like You Favorite Anime Character!!!</h1>
+   <Card className="card">
+    <h3>Baki</h3>
+    <Card.Body>
+        <video controls>
+            <source src="./videos/Baki workout.mp4" type="video/mp4"/>
+        </video>
+    </Card.Body>
+   </Card>
+   <Card className="card">
+   <h3>Zenitsu, Tanjiro, Inosuke</h3>
+    <Card.Body>
+        <video controls>
+            <source src="./videos/Demon Slayer workout.mp4" type="video/mp4"/>
+        </video>
+    </Card.Body>
+   </Card>
+   <Card className="card">
+    <h3>Ippo</h3>
+    <Card.Body>
+        <video controls>
+            <source src="./videos/Ippo workout.mp4" type="video/mp4"/>
+        </video>
+    </Card.Body>
+   </Card>
+   <Card className="card">
+    <h3>Rock Lee</h3>
+    <Card.Body>
+        <video controls>
+            <source src="./videos/Rock Lee workout.mp4" type="video/mp4"/>
+        </video>
+    </Card.Body>
+    </Card>
+   </>
+    )
 }
 
 export default AnimeWorkout;
